f3bc2f671fadc4d122669d2a829c1793e67b0f67
Yhan Montaño <yhan.carlos2001@gmail.com>
2023-01-05 12:05:52 -0400

    feat: add getById query to recipes router

--- src/server/trpc/router/recipes.ts
+++ src/server/trpc/router/recipes.ts
@@ -1,31 +1,75 @@
+import type { PrismaClient } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { cuidSchema } from "~/schema/common";
 import { recipeCreateSchema, recipeUpdateSchema } from "~/schema/recipe";
 import { protectedProcedure, router } from "../trpc";
 
+const subRecipesInclude = {
+  include: {
+    recipe: {
+      include: {
+        subRecipes: true,
+        ingredients: { include: { ingredient: true } },
+      },
+    },
+  },
+};
+
+const recipesQuery = Prisma.validator<Prisma.RecipeArgs>()({
+  include: {
+    ingredients: {
+      include: { ingredient: true },
+    },
+    subRecipes: subRecipesInclude,
+  },
+});
+
+type RecipeIngredients = Prisma.RecipeGetPayload<
+  typeof recipesQuery
+>["ingredients"];
+type RecipeSubrecipes = Prisma.RecipeGetPayload<
+  typeof recipesQuery
+>["subRecipes"];
+
+function getIngredientsCost(ingredients: RecipeIngredients) {
+  return ingredients.reduce(
+    (ac, ingredient) => ac + ingredient.ingredient.unitPrice * ingredient.units,
+    0
+  );
+}
+
+async function getSubrecipesCost(
+  subRecipes: RecipeSubrecipes,
+  prisma: PrismaClient
+): Promise<number> {
+  let total = 0;
+  for (const subRecipe of subRecipes) {
+    total += getIngredientsCost(subRecipe.recipe.ingredients);
+
+    if (subRecipe.recipe.subRecipes.length === 0) {
+      continue;
+    }
+
+    const subrecipeSubrecipes = await prisma.subRecipe.findMany({
+      ...subRecipesInclude,
+      where: {
+        parentRecipeId: subRecipe.recipe.id,
+      },
+    });
+
+    total += await getSubrecipesCost(subrecipeSubrecipes, prisma);
+  }
+  return total;
+}
+
 export const recipeRouter = router({
   getAll: protectedProcedure
     .input(z.object({ search: z.string().max(32).optional() }).optional())
     .query(async ({ ctx, input }) => {
-      const subRecipesInclude = {
-        include: {
-          recipe: {
-            include: {
-              subRecipes: true,
-              ingredients: { include: { ingredient: true } },
-            },
-          },
-        },
-      };
-
       const recipes = await ctx.prisma.recipe.findMany({
-        include: {
-          ingredients: {
-            include: { ingredient: true },
-          },
-          subRecipes: subRecipesInclude,
-        },
+        ...recipesQuery,
         where: {
           userId: ctx.session.user.id,
           ...(input?.search && {
@@ -37,7 +81,10 @@ export const recipeRouter = router({
       const recipesWithPrices = [];
       for (const recipe of recipes) {
         const ingredientsCost = getIngredientsCost(recipe.ingredients);
-        const subRecipesCost = await getSubrecipesCost(recipe.subRecipes);
+        const subRecipesCost = await getSubrecipesCost(
+          recipe.subRecipes,
+          ctx.prisma
+        );
         const cost = ingredientsCost + subRecipesCost;
         const price = cost * (recipe.profitPercentage / 100 + 1);
         recipesWithPrices.push({
@@ -50,39 +97,6 @@ export const recipeRouter = router({
       }
 
       return recipesWithPrices;
-
-      function getIngredientsCost(
-        ingredients: typeof recipes[number]["ingredients"]
-      ) {
-        return ingredients.reduce(
-          (ac, ingredient) =>
-            ac + ingredient.ingredient.unitPrice * ingredient.units,
-          0
-        );
-      }
-
-      async function getSubrecipesCost(
-        subRecipes: typeof recipes[number]["subRecipes"]
-      ): Promise<number> {
-        let total = 0;
-        for (const subRecipe of subRecipes) {
-          total += getIngredientsCost(subRecipe.recipe.ingredients);
-
-          if (subRecipe.recipe.subRecipes.length === 0) {
-            continue;
-          }
-
-          const subrecipeSubrecipes = await ctx.prisma.subRecipe.findMany({
-            ...subRecipesInclude,
-            where: {
-              parentRecipeId: subRecipe.recipe.id,
-            },
-          });
-
-          total += await getSubrecipesCost(subrecipeSubrecipes);
-        }
-        return total;
-      }
     }),
 
   create: protectedProcedure
@@ -168,5 +182,52 @@ export const recipeRouter = router({
       return await ctx.prisma.recipe.delete({
         where: { id: input.id, userId: ctx.session.user.id },
       });
+    }),
+
+  getById: protectedProcedure
+    .input(z.object({ recipeId: cuidSchema }))
+    .query(async ({ ctx, input }) => {
+      const recipe = await ctx.prisma.recipe.findUnique({
+        include: {
+          ingredients: {
+            include: { ingredient: { include: { measurementUnit: true } } },
+          },
+          subRecipes: subRecipesInclude,
+        },
+        where: { id: input.recipeId, userId: ctx.session.user.id },
+      });
+
+      if (!recipe) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const ingredients = recipe.ingredients.map((ingredient) => ({
+        ...ingredient,
+        cost: ingredient.ingredient.unitPrice * ingredient.units,
+      }));
+
+      const subRecipes = [];
+      for (const subrecipe of recipe.subRecipes) {
+        subRecipes.push({
+          ...subrecipe,
+          cost: await getSubrecipesCost([subrecipe], ctx.prisma),
+        });
+      }
+
+      const totalCost =
+        ingredients.reduce((ac, ingredient) => ac + ingredient.cost, 0) +
+        subRecipes.reduce((ac, subrecipe) => ac + subrecipe.cost, 0);
+
+      const price = totalCost * (recipe.profitPercentage / 100 + 1);
+
+      const netIncome = price - totalCost;
+      return {
+        ...recipe,
+        ingredients,
+        subRecipes,
+        totalCost,
+        price,
+        netIncome,
+      };
     }),
 });
